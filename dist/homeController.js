45b9c4ad7e3b9e1d4aa387319ae45115fd9e2798
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-02 16:03:27 +0900

    12.3 ルーティング

---
+++ dist/homeController.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.render = void 0;
+const render = (viewName, req, res) => {
+    res.render(viewName);
+};
+exports.render = render;
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import * as homeController from './homeController';
 
 const app = express();
 app.set('port', process.env['WEB_APP_PORT'] || 3000);
 
-app.get('/', (req, res) => {
-  res.send('ようこそコンフェッティ・キュイジンへ');
-})
-
 // analyze request body
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json());
 
-app.listen(app.get('port'), () => {
-  console.log(`listening... port: ${app.get('port')}`);
-});
+// route
+app
+  .get('/', (req, res) => res.send('ようこそコンフェッティ・キュイジンへ'))
+  .get('/courses', (req, res) => homeController.render('courses', req, res))
+  .get('/contact', (req, res) => homeController.render('contact', req, res))
+  .post('/contact', (req, res) => homeController.render('thanks', req, res))
+
+app.listen(app.get('port'), () => console.log(`listening... port: ${app.get('port')}`));
